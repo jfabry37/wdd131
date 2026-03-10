403af7223adf2ce658092d81ff7c0091d1ead528
jfabry37 <jrc80525@yahoo.com>
2026-03-09 20:47:41 -0700

    javascript added

---
+++ scripts/getdates.js
@@ -0,0 +1,12 @@
+const year = document.querySelector('#currentYear');
+const today = new Date();
+year.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
+
+
+
+const short = document.getElementById("lastModified").innerHTML = document.lastModified;
+
+document.getElementById("lastModified").innerHTML = document.lastModified;
+
+
+// id="lastModified" short modification
