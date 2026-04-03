f9c3db63576296cd0e372c9eb98aff7fdc6ff3f7
jfabry37 <jrc80525@yahoo.com>
2026-04-02 20:02:10 -0700

    week05 form

---
+++ scripts/form.js
@@ -0,0 +1,8 @@
+
+
+
+
+const year = document.querySelector('#currentYear');
+const today = new Date();
+year.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
+const short = document.getElementById("lastModified").innerHTML = document.lastModified;
